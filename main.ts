c7dd2c234e9736f3f791aa783f2d420b54f7b73a
iiy4383 <znlspf123@naver.com>
2021-06-28 15:38:24 +0900

    20210628 Array & Objects

--- main.ts
+++ main.ts
@@ -1,15 +1,18 @@
 // Arrays
-var names = ['Lee', "Shin", "Kim", "Park", "Ryu"];
-var favorites = [2.4, 6, 8.1, 10.4, 9.0];
-var chaoticSomething = ['9', 6.1, '9.6', 7, 239847];
+let names = ['Lee', "Shin", "Kim", "Park", "Ryu"];
+let favorites = [2.4, 6, 8.1, 10.4, 9.0];
+let chaoticSomething = ['9', 6.1, '9.6', 7, 239847];
+
 // let chaoticSomething[0] = 7;     // 배열 구조 파괴 패턴 필요
 // let chaoticSomething[1] = '6.4'; // 배열 구조 파괴 패턴 필요
+
 // Objects
-var myInfo = {
+let myInfo = {
     name: "Leejong",
     age: 27,
     sex: "Male",
     isBlack: false
-};
+}
+
 myInfo.name = "Shin Leejong";
 // myInfo.age = "28";  // 할당 불가
---
+++ readme.md
@@ -0,0 +1,6 @@
+## TypeScript Tutorial by The Net Ninja
+Start - June 28, 2021 <br />
+TypeScript를 사용할 줄 모르는 건 아니나, 다시 단단하게 개념을 다지기 위해
+처음부터 다시 출발합니다.
+<br /><br />
+\#4 - Objects & Arrays
